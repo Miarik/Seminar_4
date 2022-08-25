a6f6ba099dc2d8edb7a1f53487e95a86a7b60430
Olga <miarik@yandex.ru>
2022-08-25 11:29:52 -0400

    Exercise 28

---
+++ Exercise_26/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+
+char[] arr = number.ToCharArray();
+
+int GetArrayLength (char[] array)
+{
+    int count = array.Length;
+    return count;
+}
+
+Console.WriteLine(GetArrayLength(arr));
---
+++ Exercise_28/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberMultiplication(int num) 
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * num;
+    }
+    return mult;
+}
+Console.WriteLine(NumberMultiplication(number));
